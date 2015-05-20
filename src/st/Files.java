a30f2218609d1e9144f4b87e2731cc1741c5748d
Назар <@>
2015-05-20 23:56:49 +0300

    Files

--- src/st/Files.java
+++ src/st/Files.java
@@ -1,7 +1,11 @@
 package st;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.SecureRandom;
@@ -13,6 +17,9 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class Files {
 	public boolean downloadFile(String kontora, String webSite)
@@ -50,7 +57,21 @@ public class Files {
 			e.printStackTrace();
 			return false;
 		}
-
+		
+		/*JSONObject jo = (JSONObject) new JSONTokener(IOUtils.toString(new URL("http://gdata.youtube.com/feeds/api/videos/SIFL9qfmu5U?alt=json"))).nextValue();
+		System.out.println(jo);
+		URL url2 = new URL("http://live.fonbet.com/live/currentLine/ru/?0.20472579340931407");
+	      BufferedReader reader = new BufferedReader(new InputStreamReader(url2.openStream()));
+	      BufferedWriter writer = new BufferedWriter(new FileWriter("data.html"));
+	      String line;
+	      while ((line = reader.readLine()) != null) {
+	        // System.out.println(line);
+	         writer.write(line);
+	         writer.newLine();
+	      }
+	      reader.close();
+	      writer.close();
+*/
 		return true;
 	}
 }
