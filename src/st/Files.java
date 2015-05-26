fd861a355e2abe90e9bd0aea5e1688e4e9929206
Назар <@>
2015-05-27 00:31:06 +0300

    update Files.java

--- src/st/Files.java
+++ src/st/Files.java
@@ -5,11 +5,16 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
@@ -30,24 +35,14 @@ public class Files {
 		url = new URL(webSite);
 		File destination = new File(kontora + ".html");
 
-		// =================================================
-		
-		// Create a trust manager that does not validate certificate chains
-		/*TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager(){
-		    public X509Certificate[] getAcceptedIssuers(){return null;}
-		    public void checkClientTrusted(X509Certificate[] certs, String authType){}
-		    public void checkServerTrusted(X509Certificate[] certs, String authType){}
-		}};
+	if (kontora.equalsIgnoreCase("Fonbet")){
+		 BufferedWriter writer = new BufferedWriter(new FileWriter(kontora +".html"));
 
-		// Install the all-trusting trust manager
-		try {
-		    SSLContext sc = SSLContext.getInstance("TLS");
-		    sc.init(null, trustAllCerts, new SecureRandom());
-		    HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-		} catch (Exception e) {
-		    ;
-		}*/
-		//===============================================
+         writer.write(downloadHtml(webSite));
+         writer.newLine();
+      
+      writer.close();
+	}else{
 		try {
 
 			FileUtils.copyURLToFile(url, destination);
@@ -58,20 +53,61 @@ public class Files {
 			return false;
 		}
 		
-		/*JSONObject jo = (JSONObject) new JSONTokener(IOUtils.toString(new URL("http://gdata.youtube.com/feeds/api/videos/SIFL9qfmu5U?alt=json"))).nextValue();
-		System.out.println(jo);
-		URL url2 = new URL("http://live.fonbet.com/live/currentLine/ru/?0.20472579340931407");
-	      BufferedReader reader = new BufferedReader(new InputStreamReader(url2.openStream()));
-	      BufferedWriter writer = new BufferedWriter(new FileWriter("data.html"));
-	      String line;
-	      while ((line = reader.readLine()) != null) {
-	        // System.out.println(line);
-	         writer.write(line);
-	         writer.newLine();
-	      }
-	      reader.close();
-	      writer.close();
-*/
+	}  
 		return true;
 	}
+	
+	private static String downloadHtml(String urlString) {
+	    URL url = null;
+	    InputStream inStr = null;
+	    StringBuffer buffer = new StringBuffer();
+
+	    try {
+	        url = new URL(urlString);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection(); // Cast shouldn't fail
+	        HttpURLConnection.setFollowRedirects(true);
+	        // allow both GZip and Deflate (ZLib) encodings
+	        //conn.setRequestProperty("Accept-Encoding", "gzip, deflate"); 
+	        String encoding = conn.getContentEncoding();
+	        inStr = null;
+
+	        // create the appropriate stream wrapper based on
+	        // the encoding type
+	        if (encoding != null && encoding.equalsIgnoreCase("gzip")) {
+	            inStr = new GZIPInputStream(conn.getInputStream());
+	        } else if (encoding != null && encoding.equalsIgnoreCase("deflate")) {
+	            inStr = new InflaterInputStream(conn.getInputStream(),
+	              new Inflater(true));
+	        } else {
+	            inStr = conn.getInputStream();
+	        }
+	        int ptr = 0;
+
+
+	        InputStreamReader inStrReader = new InputStreamReader(inStr);
+	        //InputStreamReader inStrReader = new InputStreamReader(inStr, Charset.forName("GB2312"));
+
+	        while ((ptr = inStrReader.read()) != -1) {
+	            buffer.append((char)ptr);
+	        }
+	        inStrReader.close();
+
+	        conn.disconnect();
+	    }
+	    catch(Exception e) {
+
+	        e.printStackTrace();
+	    }
+	    finally {
+	        if (inStr != null)
+	            try {
+	                inStr.close();
+	            } catch (IOException e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+	            }
+	    }
+
+	    return buffer.toString();
+	  }
 }
