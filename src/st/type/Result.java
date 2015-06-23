91df45919160593aa25c5b43deedeb86f0d22bca
Назар <@>
2015-06-23 19:24:40 +0200

    update Result

--- src/st/type/Result.java
+++ src/st/type/Result.java
@@ -3,6 +3,10 @@ package st.type;
 public class Result {
 	public boolean fork;
 	public String nameOfKoefKontora1;
+	public String nameOfCommand1Kontora1;
+	public String nameOfCommand2Kontora1;
+	public String nameOfCommand1Kontora2;
+	public String nameOfCommand2Kontora2;
 	public float KoefKontora1;
 	public String nameOfKoefKontora2;
 	public float KoefKontora2;
